9604365391388479699ddeda82496122cac023be
PanQuixote <1448865036@qq.com>
2019-01-16 11:01:04 +0800

    v0.7

--- main.cpp
+++ main.cpp
@@ -1,38 +1,53 @@
 #include "libraryfile_and_define.h"
 #include "class.h"
-
+#include"calculate_tool_class.h"
 int main()
 {
-	generator G;
-	for (int i = 1; i <= 50; i++)
+	//generator G;
+	//for (int i = 1; i <= 50; i++)
+	//{
+	//	formula F = G.get_formula();//显示模式参数为默认的0，乘方显示为^
+	//	//调用get_formula函数后会将获取到的式子追加输入到文件problem_file.txt中
+
+
+	//	cout << "题目 " << F.id << endl;
+	//	cout << F.problem << endl;
+	//	cout << "答案：";
+	//	cout << F.answer << endl << endl;
+
+	//	//cout << "输入答案：";
+	//	//string answer;
+	//	//cin >> answer;
+
+	//	//if (G.check_answer(F.id, answer) == 1)//检查答案是否正确，正确返回1
+	//	//	cout << "回答正确" << endl << endl;
+	//	//else
+	//	//	cout << "回答错误" << endl << endl;
+	//}
+
+	//for (int i = 51; i <= 100; i++)
+	//{
+	//	formula F = G.get_formula(1);//显示模式参数为1，乘方显示为**
+
+	//	cout << "题目 " << F.id << endl;
+	//	cout << F.problem << endl;
+	//	cout << "答案：";
+	//	cout << F.answer << endl << endl;
+	//}
+
+	string str[100] = {
+		"1 + 2 ",
+		"1 / 0",
+		"4/5 + 3/5",
+		"1/3 + 1/4",
+		"1 + 1/5"
+	};
+	
+	calculate_tool C_tool;
+	for (int i = 0; i < 5; i++)
 	{
-		formula F = G.get_formula();//显示模式参数为默认的0，乘方显示为^
-		//调用get_formula函数后会将获取到的式子追加输入到文件problem_file.txt中
-
-
-		cout << "题目 " << F.id << endl;
-		cout << F.problem << endl;
-		cout << "答案：";
-		cout << F.answer << endl << endl;
-
-		//cout << "输入答案：";
-		//string answer;
-		//cin >> answer;
-
-		//if (G.check_answer(F.id, answer) == 1)//检查答案是否正确，正确返回1
-		//	cout << "回答正确" << endl << endl;
-		//else
-		//	cout << "回答错误" << endl << endl;
-	}
-
-	for (int i = 51; i <= 100; i++)
-	{
-		formula F = G.get_formula(1);//显示模式参数为1，乘方显示为**
-
-		cout << "题目 " << F.id << endl;
-		cout << F.problem << endl;
-		cout << "答案：";
-		cout << F.answer << endl << endl;
+		word answer = C_tool.calculate_suffix(str[i]);
+		cout << answer.str_word() << endl << endl;
 	}
 
 	system("pause");
